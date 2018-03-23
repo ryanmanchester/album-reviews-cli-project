71b0cae6a24e208da92aac245ac6d6532bb0a5f3
Ryan Manchester <ryanm17@gmail.com>
2018-03-23 05:35:19 +0000

    added 'detect' as an iterator

--- album_reviews/lib/album_reviews/reviews.rb
+++ album_reviews/lib/album_reviews/reviews.rb
@@ -1,5 +1,5 @@
 class AlbumReviews::Reviews
-  attr_accessor :artist, :album_name, :author, :label, :publication_date, :rating, :url
+  attr_accessor :artist, :album, :author, :label, :date, :rating, :url
   @@all = []
 
   def self.all
@@ -8,19 +8,19 @@ class AlbumReviews::Reviews
 
   def self.review_index_scrape
     doc = Nokogiri::HTML(open("http://www.undertheradarmag.com/reviews/category/music/"))
-    binding.pry
+    #binding.pry
     #artist(featured) = doc.css(".headline").children.css("a").children[0].text
-    #artist =  doc.css(".headline h3 a").children  *iterate over all names*
-    #album_name = doc.css(".headline h4 i a").children  *iterate over these*
-    #label = doc.css(".headline h5").children
-    #publication_date = doc.css(".date").children
-    #url = doc.css(".headline a").attribute("href").value
-
+    #  review = self.new
+    # artist =  doc.css(".headline h3 a").children 
+    # album = doc.css(".headline h4 i a").children  
+    # label = doc.css(".headline h5").children
+    # date = doc.css(".date").children
+    # url = doc.css(".headline a").attribute("href").value  
   end
 
   def self.review_profile_scrape
     doc = Nokogiri::HTML(open("http://www.undertheradarmag.com/reviews/david_byrne_american_utopia/"))
-    #binding.pry
+    binding.pry
     #author = doc.css(".more-details").children.css("span").children.text
     #rating = doc.css("#rating b").children.text
 
@@ -29,11 +29,13 @@ class AlbumReviews::Reviews
   #iteration code to assign artist, album, label (using artist as example):
   #artist_name = doc.css(".headline h3 a").children
   #artist_name.each do |name|
-  #  if @artist == nil
-  #    @artist = name.text
+  #  if review.artist == nil
+  #    review.artist = name.text
   #  else
-  #   @artist
+  #   review.artist
   #  end
   # end
+
+  #doc.css(".teaser a").attribute("href").value
 
 end
